956029edcbfcc16594c3424ccf1fe4b42d82fd94
Arafat <arafathossin7616@gmail.com>
2023-09-10 18:39:24 +0600

    UPDATE BRAND AND CONVERT TO CLIENT COMPONENT

--- components/Brands.js
+++ components/Brands.js
@@ -1,12 +1,38 @@
-import { fetchData } from "@/utils/fetchData";
-import BrandSlider from "./elements/sliders/Brands";
-const Brands = async () => {
-  const data = await fetchData({ api: "brands" });
-  const brands = data?.data || [];
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+
+import { useGetBrandsQuery } from "@/store/features/api/brandsAPI";
+
+
+const Brands = () => {
+  const { data: brandsData } = useGetBrandsQuery();
+  console.log('brandsData-------', brandsData);
+
+  const brands = brandsData?.data || [];
 
   return (
     <>
-      <BrandSlider brands={brands} />
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-5">
+        {brands?.map((brand, i) => (
+          <div class="mb-5">
+            <Link
+              href={`/brands/${brand.id}`}
+              className=""
+            >
+              <Image
+                src={brand.brand_image || noImage}
+                alt={brand.title}
+                width={112}
+                height={112}
+                className="w-[100%] h-[100%] object-cover shadow-lg"
+              />
+            </Link>
+          </div>
+        ))}
+
+      </div>
+
     </>
   );
 };
