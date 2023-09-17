794f8f51ab16125e26b5c75d3ba2b15bee49dc7c
Arafat <arafathossin7616@gmail.com>
2023-09-17 17:10:58 +0600

    404 page updated

--- app/[locale]/not-found.js
+++ app/[locale]/not-found.js
@@ -9,14 +9,14 @@ export default function NotFound() {
         <>
             <div className="container">
                 <div className="content flex flex-col items-center py-28">
-                    <Image src={`/assets/images/banner/404.png`} width={340} height={340} alt="Not Found" className="mb-12"/>
-                    <h2 className="text-3xl font-bold font-title text-slate-900">পৃষ্ঠা খুঁজে পাওয়া যায়নি!</h2>
-                    <p className="text-lg text-slate-600 mt-6 mb-8">আমরা দুঃখিত, কিন্তু আপনার অনুরোধ করা পৃষ্ঠাটি পাওয়া যায়নি</p>
-                    <Link href="/" className="inline-block font-semibold text-white bg-primary rounded-lg px-8 py-3">হোমে ফিরে যান</Link>
+                    <Image src={`/assets/images/banner/notFound.jpg`} width={600} height={500} alt="Not Found" className="mb-12" />
+                    <h2 className="text-3xl font-bold font-title text-slate-900">Page not found!</h2>
+                    <p className="text-lg text-slate-600 mt-6 mb-8">We're sorry, but the page you requested could not be found.</p>
+                    <Link href="/" className="inline-block font-semibold text-white bg-primary px-8 py-3">Back to home</Link>
                 </div>
-                <div className="contact pb-12 text-center">
-                    <p className="flex justify-center items-center gap-5 bg-amber-200 border border-primary rounded-xl p-4"><span className="text-xl font-bold font-title text-slate-900">যে কোন জিজ্ঞাসা বা অর্ডার করতে আমাদের কল করুন:</span> <Link href="tel:01720060958" className="text-xl font-bold font-title text-primary"><BsFillTelephoneFill/> 01720060958</Link></p>
-                </div>
+                {/* <div className="contact pb-12 text-center">
+                    <p className="flex justify-center items-center gap-5 bg-amber-200 border border-primary rounded-xl p-4"><span className="text-xl font-bold font-title text-slate-900">Call us for any inquiry or order:</span> <Link href="tel:01720060958" className="text-xl font-bold font-title text-primary"><BsFillTelephoneFill/> 01720060958</Link></p>
+                </div> */}
             </div>
         </>
     );
