6d0e7a06d73bdac8f0fd61a680bdc8f3cf8e18c0
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2023-09-19 13:11:04 +0600

    product details adjusted

--- app/[locale]/products/[slug]/ProductDetails.js
+++ app/[locale]/products/[slug]/ProductDetails.js
@@ -12,17 +12,14 @@ import { getSlicedText } from "@/utils/formatText";
 import ViewHTML from "@/components/elements/ViewHTML";
 import { addToCart } from "@/store/features/cartSlice";
 import ProductVariantSelect from "@/components/products/ProductVariantSelect";
-import ActiveLink from "@/components/elements/ActiveLink";
-import { Rating } from "react-simple-star-rating";
-import { formatLongNumber, getFractionFixed } from "@/utils/formatNumber";
-import SocialShare from "@/components/elements/SocialShare";
 
 // ** Import Icon
-import { HiChatBubbleLeftRight, HiOutlineShoppingCart } from "react-icons/hi2";
+import { HiOutlineShoppingCart } from "react-icons/hi2";
 import { TbTag } from "react-icons/tb";
 import { IoCall, IoCopy } from "react-icons/io5";
+import { BsFillTelephoneFill } from "react-icons/bs";
 
-const ProductDetails = ({ children, product, tabItems, settings }) => {
+const ProductDetails = ({ product, settings }) => {
   const [selectedVariant, setSelectedVariant] = useState(null);
   const dispatch = useDispatch();
   const router = useRouter();
@@ -54,47 +51,6 @@ const ProductDetails = ({ children, product, tabItems, settings }) => {
           <div className="w-1/2">
             <div className="sticky top-4">
               <ThumbSlider product={product} />
-              <div className="py-4">
-                <div className="product-actions my-6 flex gap-4 justify-between items-center">
-                  <button
-                    className="bg-secondary-700 py-3 w-full px-6 text-white rounded-lg text-center active:scale-95"
-                    onClick={handleAddToCart}
-                  >
-                    <HiOutlineShoppingCart size={24} />
-                    <span className="ml-2">কার্টে রাখুন</span>
-                  </button>
-                  <button
-                    onClick={handleBuyNow}
-                    className="bg-primary py-3 w-full px-6 text-white rounded-lg text-center active:scale-95"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      viewBox="0 0 20 20"
-                      fill="none"
-                    >
-                      <path
-                        d="M7.91634 1.66667H15.4163L10.833 7.50001H17.083L7.08301 18.3333L9.16634 10.4167H3.33301L7.91634 1.66667Z"
-                        fill="white"
-                        stroke="white"
-                        stroke-linejoin="round"
-                      />
-                    </svg>{" "}
-                    <span className="mr-2">এখনই কিনুন</span>
-                  </button>
-                </div>
-                <div className="border-t border-slate-200 rounded-lg flex justify-between items-center flex-wrap py-4 font-bold">
-                  <p className="text-slate-900">বিস্তারিত জানতে কল করুন</p>
-                  <p className="text-primary">
-                    <IoCall /> {settings?.phone}
-                  </p>
-                  <p className="text-slate-500">অথবা</p>
-                  <p className="text-primary">
-                    <IoCall /> {settings?.phone}
-                  </p>
-                </div>
-              </div>
             </div>
           </div>
           <div className="w-1/2">
@@ -102,10 +58,23 @@ const ProductDetails = ({ children, product, tabItems, settings }) => {
               <p className="text-sm font-bold text-primary capitalize mb-2">
                 {product?.brand?.brand_name || "No Brand"}
               </p>
-              <h5 className="text-2xl font-bold font-title text-slate-900">
+              <h5 className="text-2xl font-title text-slate-900">
                 {getSlicedText(product?.product_name, 100)}
               </h5>
-              <div className="meta-data flex items-center gap-8 my-2">
+              <div className="product-price flex items-center gap-4 py-4">
+                <span className="text-xl font-title text-slate-900">
+                  tk. {product?.new_price || "0.00"}{" "}
+                </span>
+                {product?.discount_percentage > 0 ? (
+                  <>
+                    <del className="old-price text-lg/[24px] font-normal text-slate-400">
+                      {product?.old_price ? `${product?.old_price}` : "0.00"}
+                    </del>
+                  </>
+                ) : null}
+              </div>
+              {/* Rating Review and Share section  */}
+              {/* <div className="meta-data flex items-center gap-8 my-2">
                 <div className="flex gap-1 items-center">
                   <Rating
                     initialValue={product?.averate_rating || 5}
@@ -126,28 +95,12 @@ const ProductDetails = ({ children, product, tabItems, settings }) => {
                   প্রশ্ন এবং উত্তর
                 </p>
                 <SocialShare />
-              </div>
+              </div> */}
               {/* short description  */}
               <ViewHTML
                 htmlText={product?.product_short_description}
                 className={"desc text-base text-slate-600"}
               />
-              <div className="product-price flex items-center gap-4 border-b border-slate-200 py-5">
-                <span className="text-3xl/[48px] font-bold font-title text-slate-900">
-                  ৳ {product?.new_price || "0.00"}{" "}
-                </span>
-                {product?.discount_percentage > 0 ? (
-                  <>
-                    <del className="old-price text-lg/[24px] font-normal text-slate-400">
-                      ৳{" "}
-                      {product?.old_price ? `$ ${product?.old_price}` : "0.00"}
-                    </del>
-                    <span className="discount inline-block text-base font-semibold font-title text-white bg-red-500 rounded-md py-1 px-2">
-                      - {getFractionFixed(product?.discount_percentage)}%
-                    </span>
-                  </>
-                ) : null}
-              </div>
               {product?.productVariants?.length ? (
                 <ProductVariantSelect
                   productVariants={product?.productVariants}
@@ -199,86 +152,89 @@ const ProductDetails = ({ children, product, tabItems, settings }) => {
                   </Link>
                 </div>
               ) : null}
-              <div className="delivery flex flex-wrap gap-y-7 bg-slate-50 border border-slate-200 rounded-lg py-4">
-                <div className="single-info">
-                  <Image
-                    src={`/assets/images/icons/inside.png`}
-                    alt=""
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    className="w-full"
-                  />
-                  <div>
-                    <p className="text-slate-600">ডেলিভারি খরচ:</p>
-                    <p className="text-slate-600">
-                      ঢাকার ভিতরে - {settings?.inside_dhaka_delivery_charges}{" "}
-                      টাকা
-                    </p>
-                  </div>
-                </div>
-                <div className="single-info border-l border-slate-200">
-                  <Image
-                    src={`/assets/images/icons/outside.png`}
-                    alt=""
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    className="w-full"
-                  />
-                  <div>
-                    <p className="text-slate-600">ডেলিভারি খরচ:</p>
-                    <p className="text-slate-600">
-                      ঢাকার বাইরে - {settings?.outside_dhaka_delivery_charges}{" "}
-                      টাকা
-                    </p>
-                  </div>
-                </div>
-                <div className="single-info">
-                  <Image
-                    src={`/assets/images/icons/COD.png`}
-                    alt=""
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    className="w-full"
-                  />
-                  <div>
-                    <p className="text-slate-600">
-                      সারাদেশ এ ক্যাশ অন ডেলিভারি
-                    </p>
-                  </div>
-                </div>
-                <div className="single-info border-l border-slate-200">
-                  <Image
-                    src={`/assets/images/icons/paymnt.png`}
-                    alt=""
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    className="w-full"
-                  />
-                  <div>
-                    <p className="text-slate-600">
-                      নিরাপদ পেমেন্ট করার সহজ মাধ্যম
-                    </p>
-                  </div>
-                </div>
+            </div>
+
+            {/* Add to cart section  */}
+            <div className="py-4">
+              <div className="product-actions my-6 flex gap-4 justify-between items-center">
+                <button
+                  className="bg-secondary-700 py-3 w-full px-6 text-white rounded-lg text-center active:scale-95"
+                  onClick={handleAddToCart}
+                >
+                  <HiOutlineShoppingCart size={24} />
+                  <span className="ml-2">কার্টে রাখুন</span>
+                </button>
+                <button
+                  onClick={handleBuyNow}
+                  className="bg-primary py-3 w-full px-6 text-white rounded-lg text-center active:scale-95"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                  >
+                    <path
+                      d="M7.91634 1.66667H15.4163L10.833 7.50001H17.083L7.08301 18.3333L9.16634 10.4167H3.33301L7.91634 1.66667Z"
+                      fill="white"
+                      stroke="white"
+                      stroke-linejoin="round"
+                    />
+                  </svg>{" "}
+                  <span className="mr-2">এখনই কিনুন</span>
+                </button>
               </div>
             </div>
-            {/* tabs-view previous position */}
-            <div className="tabs-view">
-              {/* tabs view */}
-              <ul className="sticky top-0 bg-white z-20 product-tab-links flex justify-between items-center border-b border-slate-200 py-5 mt-5">
-                {tabItems.map((item) => (
-                  <li key={item.id}>
-                    <ActiveLink href={item.path}>{item.title}</ActiveLink>
-                  </li>
-                ))}
-              </ul>
-              {/* tabs content  */}
-              <div className="product-tab-content pt-8 pb-4 border-b-4 border-slate-200">
-                {children}
+
+            {/* Product Descriptions */}
+            <div className="pt-8 pb-4">
+              <div className="description">
+                <h4 className="text-2xl font-bold font-title text-slate-900">
+                  Description:
+                </h4>
+                <ViewHTML htmlText={product?.details} />
+              </div>
+              <div className="mt-8">
+                <h4 className="text-2xl font-bold font-title text-slate-900 mb-4">
+                  Product Included
+                </h4>
+                <Image
+                  src={
+                    product?.product_includes ||
+                    `/assets/images/shop/accessories.jpg`
+                  }
+                  alt="Insta 360"
+                  width={628}
+                  height={510}
+                  className="w-full h-[510px] rounded-xl"
+                />
+              </div>
+
+              {product?.review_video && (
+                <div className="mt-8">
+                  <h4 className="text-2xl font-bold font-title text-slate-900">
+                    Review Video
+                  </h4>
+                  <div className="slider-imag [&>div>iframe]:rounded-xl relative mt-4">
+                    <ViewHTML htmlText={product?.review_video} />
+                  </div>
+                </div>
+              )}
+
+              <div className="contact mt-8 bg-amber-200 border border-primary rounded-xl p-5 mb-4 text-center">
+                <h5 className="text-2xl font-bold font-title text-slate-900 mb-3">
+                  Contact for more details
+                </h5>
+                <p className="flex justify-center items-center gap-4">
+                  <span className="text-base text-slate-900">কল করুন:</span>{" "}
+                  <Link
+                    href={`tel:${settings?.phone}`}
+                    className="text-2xl font-bold font-title text-primary"
+                  >
+                    <BsFillTelephoneFill /> {settings?.phone}
+                  </Link>
+                </p>
               </div>
             </div>
           </div>
