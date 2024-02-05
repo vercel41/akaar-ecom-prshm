2fddfb646bcbff428cbec5d12066c906efff9d8a
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2024-02-05 15:38:25 +0600

    loading image adjusted

--- app/[locale]/loading.jsx
+++ app/[locale]/loading.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import loading from "@/public/assets/images/loading.gif";
 
 export default function Loading() {
 	return (
 		<div className="flex h-screen w-screen justify-center items-center -mt-16">
 			<Image
-				className={`h-[300px] w-[300px]`}
-				src={"/assets/images/loading-snail.gif"}
+				className={`h-[200px] w-[200px]`}
+				src={"https://akaarserver.xyz/loading.gif" || loading}
 				alt={"loader"}
-				width={226}
-				height={226}
+				width={200}
+				height={200}
 			/>
 		</div>
 	);
