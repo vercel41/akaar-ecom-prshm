622bbe6553b08156fd7e5fab5e25b29fb69b7e2d
Hasan Mohammad <89174358+hasan-mohammad-bd@users.noreply.github.com>
2024-02-13 02:14:13 -0800

    Added Zoom in product details and increase timing  (#90)
    * added zoom
    
    * remove console log
    
    ---------
    
    Co-authored-by: Arafat Hossin <63033297+Arafat7616@users.noreply.github.com>

--- app/[locale]/products/[slug]/_components/ProductViewSlider.js
+++ app/[locale]/products/[slug]/_components/ProductViewSlider.js
@@ -5,95 +5,90 @@ import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Thumbs, Autoplay } from "swiper/modules";
 import noImage from "@/public/assets/images/no-image.png";
+import ImageZoom from "./ImageZoom";
 
 // ** Import Icon
 import { HiOutlineHeart } from "react-icons/hi2";
 import useWishList from "@/hooks/useWishList";
 
 const ProductViewSlider = ({ product, settings }) => {
-	const [thumbsSwiper, setThumbsSwiper] = useState(null);
-	const {
-		handleAddToWishlist,
-		handleWishListProductStatus,
-		handleRemoveFromWishlist,
-	} = useWishList();
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const {
+    handleAddToWishlist,
+    handleWishListProductStatus,
+    handleRemoveFromWishlist,
+  } = useWishList();
 
-	//setting default image if no image is provided
-	const photos = product?.photos?.length ? product?.photos : [noImage];
-	const isInWishlist = handleWishListProductStatus(product?.id);
+  //setting default image if no image is provided
+  const photos = product?.photos?.length ? product?.photos : [noImage];
+  const isInWishlist = handleWishListProductStatus(product?.id);
 
-	return (
-		<>
-			<div className="slider">
-				<div className="thumb-slider">
-					<Swiper
-						onSwiper={setThumbsSwiper}
-						autoplay={{
-							delay: 2500,
-							disableOnInteraction: false,
-						}}
-						direction="vertical"
-						slidesPerView={"auto"}
-						breakpoints={{
-							0: {
-								direction: "horizontal",
-							},
-							768: {
-								direction: "vertical",
-							},
-						}}
-						modules={[Thumbs, Autoplay]}
-					>
-						{photos.map((slide, index) => (
-							<SwiperSlide key={index}>
-								<div className="slider-image cursor-pointer">
-									<Image
-										src={slide}
-										alt=""
-										width={64}
-										height={64}
-										className="border object-contain h-16 w-16 cursor-pointer mb-3"
-									/>
-								</div>
-							</SwiperSlide>
-						))}
-					</Swiper>
-				</div>
 
-				<div className="preview-slider grid mx-4 relative border border-slate-200">
-					<Swiper
-						thumbs={{
-							swiper:
-								thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
-						}}
-						direction="horizontal"
-						autoplay={{
-							delay: 2500,
-							disableOnInteraction: false,
-						}}
-						slidesPerView={1}
-						breakpoints={{
-							0: {
-								direction: "horizontal",
-							},
-							768: {
-								direction: "horizontal",
-							},
-						}}
-						modules={[Thumbs, Autoplay]}
-					>
-						{photos.map((slide, index) => (
-							<SwiperSlide key={index}>
-								<div className="slider-imag h-[300px] lg:h-[36rem] w-full lg:w-[32rem]">
-									<Image
-										src={slide}
-										alt=""
-										width={524}
-										height={524}
-										// sizes="100vw"
-										className="h-full w-full object-contain rounded-lg"
-									/>
-									{/* {true && (
+  return (
+    <>
+      <div className="slider">
+        <div className="thumb-slider">
+          <Swiper
+            onSwiper={setThumbsSwiper}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            direction="vertical"
+            slidesPerView={"auto"}
+            breakpoints={{
+              0: {
+                direction: "horizontal",
+              },
+              768: {
+                direction: "vertical",
+              },
+            }}
+            modules={[Thumbs, Autoplay]}
+          >
+            {photos.map((slide, index) => (
+              <SwiperSlide key={index}>
+                <div className="slider-image cursor-pointer">
+                  <Image
+                    src={slide}
+                    alt=""
+                    width={64}
+                    height={64}
+                    className="border object-contain h-16 w-16 cursor-pointer mb-3"
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+
+        <div className="preview-slider grid mx-4 relative border border-slate-200">
+          <Swiper
+            thumbs={{
+              swiper:
+                thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null,
+            }}
+            direction="horizontal"
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: false,
+            }}
+            slidesPerView={1}
+            breakpoints={{
+              0: {
+                direction: "horizontal",
+              },
+              768: {
+                direction: "horizontal",
+              },
+            }}
+            modules={[Thumbs, Autoplay]}
+          >
+            {photos.map((slide, index) => (
+              <SwiperSlide key={index}>
+                <div className="slider-imag h-[300px] lg:h-[36rem] w-full lg:w-[32rem]">
+                  <ImageZoom image={slide} zoomImage={slide} />
+                  {/* {true && (
                     <Link
                       href="https://www.youtube.com/"
                       target="_blank"
@@ -105,33 +100,33 @@ const ProductViewSlider = ({ product, settings }) => {
                       />
                     </Link>
                   )} */}
-								</div>
-							</SwiperSlide>
-						))}
-					</Swiper>
-					{!settings?.guest_checkout ? (
-						<div className="product-action absolute top-4 right-5 z-10">
-							<button
-								aria-label="Add To Wishlist"
-								className={`action-btn inline-flex justify-center items-center w-8 h-8 border ${
-									isInWishlist
-										? "bg-primary text-white"
-										: "border-primary bg-white text-primary"
-								}`}
-								onClick={(e) =>
-									!isInWishlist
-										? handleAddToWishlist(product?.id)
-										: handleRemoveFromWishlist(product?.id)
-								}
-							>
-								<HiOutlineHeart size={18} />
-							</button>
-						</div>
-					) : null}
-				</div>
-			</div>
-		</>
-	);
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          {!settings?.guest_checkout ? (
+            <div className="product-action absolute top-4 right-5 z-10">
+              <button
+                aria-label="Add To Wishlist"
+                className={`action-btn inline-flex justify-center items-center w-8 h-8 border ${
+                  isInWishlist
+                    ? "bg-primary text-white"
+                    : "border-primary bg-white text-primary"
+                }`}
+                onClick={(e) =>
+                  !isInWishlist
+                    ? handleAddToWishlist(product?.id)
+                    : handleRemoveFromWishlist(product?.id)
+                }
+              >
+                <HiOutlineHeart size={18} />
+              </button>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProductViewSlider;
