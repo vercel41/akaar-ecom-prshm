5f6c055b2f728cc7a59ab99d43fdfde0cfd6bd6e
Muhammad Touhiduzzaman <58657283+TouhidZaman@users.noreply.github.com>
2024-06-10 17:14:17 +0600

    Added nav UI fixes (#162)
    * sub dhaka removed
    
    * searchbar UI fixed
    
    * various ui fixes

--- components/ScrollToTopButton.js
+++ components/ScrollToTopButton.js
@@ -2,40 +2,44 @@
 import React, { useState, useEffect } from "react";
 import { BiArrowToTop } from "react-icons/bi";
 
-const ScrollToTopButton = () => {
-  const [isVisible, setIsVisible] = useState(false);
+const ScrollToTopButton = ({ settings }) => {
+	const [isVisible, setIsVisible] = useState(false);
 
-  const handleScroll = () => {
-    const scrollY = window.scrollY;
-    setIsVisible(scrollY > 100);
-  };
+	const handleScroll = () => {
+		const scrollY = window.scrollY;
+		setIsVisible(scrollY > 100);
+	};
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+	};
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
-  return (
-    <>
-      {isVisible && (
-        <div
-          className="scroll-to-top-button z-50 fixed right-5 bottom-5 rounded-full shadow-lg p-5 bg-white cursor-pointer"
-          onClick={scrollToTop}
-        >
-          <BiArrowToTop size={20} />
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{isVisible && (
+				<div
+					className="scroll-to-top-button z-50 fixed right-5 bottom-5 rounded-full shadow-lg h-16 w-16 inline-flex justify-center items-center cursor-pointer"
+					onClick={scrollToTop}
+					style={{
+						backgroundColor: settings?.colors?.primary,
+						color: settings?.colors?.primary_text,
+					}}
+				>
+					<BiArrowToTop size={20} />
+				</div>
+			)}
+		</>
+	);
 };
 
 export default ScrollToTopButton;
