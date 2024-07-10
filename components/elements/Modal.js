d4c6b0c46c70cc0d0f957052b2235adec87f196f
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2024-07-10 16:08:17 +0600

    popup close button color fixed

--- components/elements/Modal.js
+++ components/elements/Modal.js
@@ -4,66 +4,81 @@ import { RiCloseCircleFill } from "react-icons/ri";
 import { useSelector } from "react-redux";
 
 export default function Modal({
-	showModal,
-	setShowModal,
-	title,
-	children,
-	bodyOnly = false,
+  showModal,
+  setShowModal,
+  title,
+  children,
+  bodyOnly = false,
 }) {
-	const { settings } = useSelector((state) => state.common);
+  const { settings } = useSelector((state) => state.common);
 
-	useLockedBody(showModal); // Lock the body when the drawer is open
+  useLockedBody(showModal); // Lock the body when the drawer is open
 
-	return (
-		<>
-			{showModal ? (
-				<>
-					{/* Backdrop */}
-					<div
-						onClick={() => bodyOnly && setShowModal(false)}
-						className={`w-full h-full opacity-40 fixed z-40 inset-0 bg-black ${
-							bodyOnly ? "cursor-pointer" : ""
-						}`}
-					></div>
-					{/* Modal */}
-					<div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto">
-						<div className={`relative w-auto mx-auto ${bodyOnly ? "p-0" : "p-5"} max-w-5xl`}>
-							{/*content*/}
-							<div className={`relative flex flex-col w-full bg-white outline-none ${bodyOnly ? "" : "rounded-lg shadow-md border-0"} overflow-hidden`}>
-								{/* Close button */}
-								<button
-									className="absolute top-0 right-0 text-4xl p-2"
-									onClick={() => setShowModal(false)}
-									style={{
-									
-										color: bodyOnly ? settings?.colors?.primary : settings?.colors?.default_text,
-									}}
-								>
-									<RiCloseCircleFill className="text-3xl md:text-4xl" />
-								</button>
-								{/*header*/}
-								{!bodyOnly && (
-									<div className="flex items-center justify-between px-2 md:px-5 py-3 md:py-3.5 mb-2">
-										<h3 className="text-[18px]/[28px] md:text-2xl font-title font-semibold text-slate-900 line-clamp-1">
-											{title ? title : null}
-										</h3>
-									</div>
-								)}
-								{/*body*/}
-								<div className={`${bodyOnly ? "" : "px-3 md:px-6 pb-3 md:pb-6"} overflow-y-auto max-h-[80vh]`}>
-									{children ? (
-										children
-									) : (
-										<p className="text-slate-500 text-lg leading-relaxed">
-											This is a regular Modal, Pass Modal children to replace it
-										</p>
-									)}
-								</div>
-							</div>
-						</div>
-					</div>
-				</>
-			) : null}
-		</>
-	);
+  return (
+    <>
+      {showModal ? (
+        <>
+          {/* Backdrop */}
+          <div
+            onClick={() => bodyOnly && setShowModal(false)}
+            className={`w-full h-full opacity-40 fixed z-40 inset-0 bg-black ${
+              bodyOnly ? "cursor-pointer" : ""
+            }`}
+          ></div>
+          {/* Modal */}
+          <div className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto">
+            <div
+              className={`relative w-auto mx-auto ${
+                bodyOnly ? "p-0" : "p-5"
+              } max-w-5xl`}
+            >
+              {/*content*/}
+              <div
+                className={`relative flex flex-col w-full bg-white outline-none ${
+                  bodyOnly ? "" : "rounded-lg shadow-md border-0"
+                } overflow-hidden`}
+              >
+                {/* Close button */}
+                <button
+                  className="absolute top-0 right-0 text-4xl p-2"
+                  onClick={() => setShowModal(false)}
+                >
+                  <RiCloseCircleFill
+                    className="text-3xl md:text-4xl"
+                    style={{
+                      color: bodyOnly
+                        ? settings?.colors?.primary || "white"
+                        : settings?.colors?.default_text,
+                    }}
+                  />
+                </button>
+                {/*header*/}
+                {!bodyOnly && (
+                  <div className="flex items-center justify-between px-2 md:px-5 py-3 md:py-3.5 mb-2">
+                    <h3 className="text-[18px]/[28px] md:text-2xl font-title font-semibold text-slate-900 line-clamp-1">
+                      {title ? title : null}
+                    </h3>
+                  </div>
+                )}
+                {/*body*/}
+                <div
+                  className={`${
+                    bodyOnly ? "" : "px-3 md:px-6 pb-3 md:pb-6"
+                  } overflow-y-auto max-h-[80vh]`}
+                >
+                  {children ? (
+                    children
+                  ) : (
+                    <p className="text-slate-500 text-lg leading-relaxed">
+                      This is a regular Modal, Pass Modal children to replace it
+                    </p>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
 }
