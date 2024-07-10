7e328f7b1fbaaa0b495b7da0a82ff95a652f677b
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2024-07-10 17:01:45 +0600

    popup url bug fixed

--- app/[locale]/_components/Popup.js
+++ app/[locale]/_components/Popup.js
@@ -1,13 +1,15 @@
 "use client";
-import Modal from "@/components/elements/Modal";
+import { cn } from "@/utils";
 import Image from "next/image";
 import { useState } from "react";
+import { Link } from "@/navigation";
+import Modal from "@/components/elements/Modal";
 
-// ** Import Iocns
-const Popup = ({ popup }) => {
+
+const Popup = ({popup}) => {
   const [showPopup, setShowPopup] = useState(!!popup?.show_website_popup);
 
-  if(!popup?.show_website_popup) return null // Don't render the popup if it's not set to show
+  if (!popup?.show_website_popup) return null; // Don't render the popup if it's not set to show
 
   return (
     <Modal
@@ -17,16 +19,25 @@ const Popup = ({ popup }) => {
       title={"Popup"}
     >
       <div className="w-[350px] md:w-[700px] 2xl:w-[900px]">
-        {popup?.popup_image && (
-          <Image
-            src={popup?.popup_image}
-            alt="popup"
-            width={900}
-            height={650}
-            style={{ aspectRatio: 'initial' }} // Fix aspect ratio
-            className="w-full h-full object-cover"
-          />
-        )}
+        <Link
+          href={popup?.popup_url || "/"}
+          onClick={(e) => !popup?.popup_url && e.preventDefault()}
+          className={cn(
+            "h-full w-full",
+            !popup?.popup_url ? "cursor-default" : "cursor-pointer"
+          )}
+        >
+          {popup?.popup_image && (
+            <Image
+              src={popup?.popup_image}
+              alt="popup"
+              width={900}
+              height={650}
+              style={{ aspectRatio: "initial" }} // Fix aspect ratio
+              className="w-full h-full object-cover"
+            />
+          )}
+        </Link>
       </div>
     </Modal>
   );
