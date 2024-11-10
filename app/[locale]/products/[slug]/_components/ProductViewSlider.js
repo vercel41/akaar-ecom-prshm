c142dc0092031ad919f95b46eb88d03c0eafd063
H M Merajul Hasan <94540374+HNMerajulhasan@users.noreply.github.com>
2024-11-10 18:03:56 +0600

    product-details-slider-done (#13)

--- app/[locale]/products/[slug]/_components/ProductViewSlider.js
+++ app/[locale]/products/[slug]/_components/ProductViewSlider.js
@@ -1,73 +1,57 @@
-"use client";
-import Image from "next/image";
-import { useDispatch } from "react-redux";
-import { forwardRef, useState, useEffect } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, EffectFade } from "swiper/modules";
-import "swiper/css/pagination";
-
-import useWishList from "@/hooks/useWishList";
-import noImage from "@/public/assets/images/no-image.png";
-import ImageZoom from "./ImageZoom";
-
-// ** Import Icon
-import { HiPlayCircle } from "react-icons/hi2";
-import { startVideoPlayer } from "@/store/slices/commonSlice";
-import { TfiAngleLeft, TfiAngleRight } from "react-icons/tfi";
-import { cn } from "@/utils";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
-import dynamic from "next/dynamic";
-import { delay } from "framer-motion";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import ProductViewSkeleton from "@/components/elements/loaders/ProductViewSkeleton";
-import ProductZoomYetAnother from "./ProductZoomYetAnother";
+'use client'
+import Image from 'next/image'
+import { useDispatch } from 'react-redux'
+import { forwardRef, useState, useEffect, useRef } from 'react'
+import noImage from '@/public/assets/images/no-image.png'
+import ProductViewSkeleton from '@/components/elements/loaders/ProductViewSkeleton'
+import ProductZoomYetAnother from './ProductZoomYetAnother'
+import { cn } from '@/utils'
 
 const ProductViewSlider = forwardRef(
   ({ product, selectedColor, isSquareImage, isLoading }, ref) => {
-    const dispatch = useDispatch();
-    const [open, setOpen] = useState(false);
-    const [index, setIndex] = useState(0);
+    const dispatch = useDispatch()
+    const [open, setOpen] = useState(false)
+    const [index, setIndex] = useState(0)
+    const imageRefs = useRef([]) // Array of refs for each large image
 
-    //setting default image if no image is provided
+    // Setting default image if no image is provided
     let slides = product?.photos?.length
       ? product?.photos
-      : [
-          {
-            image: noImage,
-          },
-        ];
+      : [{ image: noImage }]
 
-    //filtering slides based on selected color
+    // Filtering slides based on selected color
     if (selectedColor) {
       const filteredSlides = slides.filter(
-        (slide) => slide.color_name === selectedColor
-      );
-      filteredSlides.length && (slides = filteredSlides);
+        slide => slide.color_name === selectedColor
+      )
+      if (filteredSlides.length) slides = filteredSlides
     }
 
-    let colorFlag = "";
-    const isFirstItem = (colorName) => {
-      if (colorFlag !== colorName) {
-        colorFlag = colorName;
-        return true;
+    const handleOpenZoom = index => {
+      setOpen(true)
+      setIndex(index)
+    }
+
+    const handleThumbnailClick = idx => {
+      setIndex(idx)
+      // Scroll to the selected large image smoothly
+      if (imageRefs.current[idx]) {
+        imageRefs.current[idx].scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        })
       }
-      return false;
-    };
+    }
 
-    const handleOpenZoom = (index) => {
-      setOpen(true);
-      setIndex(index);
-    };
-
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
       const timer = setTimeout(() => {
-        setLoading(false);
-      }, 200);
+        setLoading(false)
+      }, 200)
 
-      return () => clearTimeout(timer);
-    }, []);
+      return () => clearTimeout(timer)
+    }, [])
 
     return (
       <>
@@ -81,65 +65,59 @@ const ProductViewSlider = forwardRef(
           />
         )}
         {!loading ? (
-          <div className="w-full">
-            <div className={cn("preview-slider grid relative w-full")}>
-              <Swiper
-                ref={ref}
-                effect={"fade"}
-                slidesPerView={1}
-                navigation={{
-                  prevEl: `.custom_prev_product-preview`,
-                  nextEl: `.custom_next_product-preview`,
-                }}
-                loop={true}
-                pagination={{
-                  clickable: true,
-                }}
-                modules={[Pagination, Navigation, EffectFade]}
-                className="slider-pagination !pb-10 product-preview-slider"
-              >
-                {slides.map((slide, index) => (
-                  <SwiperSlide
-                    onClick={() => handleOpenZoom(index)}
-                    key={index}
+          <div className='flex w-full'>
+            {/* Left Side Thumbnail List (Fixed) */}
+            <div className='sticky top-0 h-screen overflow-y-auto mr-4 no-scrollbar'>
+              <div className='flex flex-col gap-4'>
+                {slides.map((slide, idx) => (
+                  <div
+                    key={idx}
                     className={cn(
-                      "w-full slider-img cursor-pointer blur-animation"
+                      'w-18 h-28 border-2 cursor-pointer',
+                      idx === index ? 'border-blue-500' : 'border-gray-300'
                     )}
+                    onClick={() => handleThumbnailClick(idx)}
                   >
-                    <div className="h-full w-full">
-                      <Image
-                        src={slide?.image}
-                        alt=""
-                        width={524}
-                        height={524}
-                        className="object-cover h-full w-full"
-                      />
-                    </div>
-                  </SwiperSlide>
+                    <Image
+                      src={slide?.image || noImage}
+                      alt={`Thumbnail ${idx}`}
+                      width={64}
+                      height={64}
+                      className='object-cover h-full w-full'
+                    />
+                  </div>
                 ))}
-              </Swiper>
-              <div className="slider-arrow">
-                <span
-                  className={`slider-btn slider-prev slick-arrow custom_prev_product-preview !left-0 !rounded-none !w-[42px] !h-12 !bg-[#0003] !text-white`}
-                >
-                  <FaChevronLeft size={20} />
-                </span>
-                <span
-                  className={`slider-btn slider-next slick-arrow custom_next_product-preview !right-0 !rounded-none !w-[42px] !h-12 !bg-[#0003] !text-white`}
-                >
-                  <FaChevronRight size={20} />
-                </span>
               </div>
+            </div>
+
+            {/* Main Image Preview (Scrollable with hidden scrollbar) */}
+            <div className='flex-1 h-screen px-4 overflow-y-auto no-scrollbar'>
+              {slides.map((slide, idx) => (
+                <div
+                  key={idx}
+                  className='mb-8 cursor-pointer'
+                  onClick={() => handleOpenZoom(idx)}
+                  ref={el => (imageRefs.current[idx] = el)} // Assign ref to each image container
+                >
+                  <Image
+                    src={slide?.image || noImage}
+                    alt={`Image ${idx}`}
+                    width={624}
+                    height={624}
+                    className='object-cover w-full'
+                  />
+                </div>
+              ))}
             </div>
           </div>
         ) : (
           <ProductViewSkeleton />
         )}
       </>
-    );
+    )
   }
-);
+)
 
-ProductViewSlider.displayName = "ProductViewSlider";
+ProductViewSlider.displayName = 'ProductViewSlider'
 
-export default ProductViewSlider;
+export default ProductViewSlider
