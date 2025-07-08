4812e8f969148e27fefba4f5792bee262deb8ee2
apucsd <apusutradhar77@gmail.com>
2025-07-08 15:32:10 +0600

    reduce product image height

--- components/cards/ProductCard.js
+++ components/cards/ProductCard.js
@@ -72,8 +72,8 @@ const ProductCard = ({
           className={cn(
             `product-img overflow-hidden relative`,
             isSquareImage
-              ? "h-[300px] xl:h-[350px] 2xl:h-[600px]"
-              : "h-[300px] xl:h-[350px] 2xl:h-[600px]"
+              ? "h-[300px] xl:h-[350px] 2xl:h-[550px]"
+              : "h-[300px] xl:h-[350px] 2xl:h-[550px]"
           )}
         >
           <Link
