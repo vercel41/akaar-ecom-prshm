d59d61f4ae670d12332af4f526743b11573a002a
apucsd <apusutradhar77@gmail.com>
2025-07-28 15:51:48 +0600

    added: site config mobile number pattern 014

--- config/site.js
+++ config/site.js
@@ -12,8 +12,8 @@ export const siteConfig = {
     country: "Bangladesh",
     prefix: "(BD) +88",
     countryCode: "+88", // We will store phon number including first 0
-    pattern: /^(013|015|016|017|018|019)\d{8}$/,
-    patternWithCode: /^(?:\+?88)?(013|015|016|017|018|019)\d{8}$/,
+    pattern: /^(013|014|015|016|017|018|019)\d{8}$/,
+    patternWithCode: /^(?:\+?88)?(013|014|015|016|017|018|019)\d{8}$/,
   },
   email: {
     pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
