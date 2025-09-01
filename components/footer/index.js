97c662a56ac804b0cb0794c96ee73dba0591b36d
vercel-16 <vercel16@akaarit.com>
2025-09-01 14:40:33 +0600

    Update index.js

--- components/footer/index.js
+++ components/footer/index.js
@@ -211,15 +211,15 @@ const Footer = async () => {
         <div className="flex justify-between items-center border-t border-slate-50 py-4 mt-8">
           <div className="w-full">
             <div className="text-xs md:text-sm font-normal font-body text-gray-700 text-center flex flex-wrap gap-2 justify-center md:justify-start items-center">
-              <p className="text-sm">
-                {translations["copyright"]} {new Date().getFullYear()}
-              </p>
-              <p className="text-sm">
-                {translations["copyright-msg"] || "All Rights Reserved "} By
-              </p>
-              <Link href="/" className="ms-1 text-sm">
-                {settings?.name}
-              </Link>
+              // <p className="text-sm">
+              //   {translations["copyright"]} {new Date().getFullYear()}
+              // </p>
+              // <p className="text-sm">
+              //   {translations["copyright-msg"] || "All Rights Reserved "} By
+              // </p>
+              // <Link href="/" className="ms-1 text-sm">
+              //   {settings?.name}
+              // </Link>
               {/* <span className="">|</span> */}
               {/* <div className="flex  items-center gap-1">
                 <p className="text-xs md:text-sm"> Developed By</p>
