b1295798bbd4eca5b899bc9992107f8254bf22aa
vercel-16 <vercel16@akaarit.com>
2025-09-01 14:41:49 +0600

    Update PersistUser.js

--- components/utility/PersistUser.js
+++ components/utility/PersistUser.js
@@ -15,8 +15,8 @@ const PersistUser = () => {
 				dispatch(setUser(response.data.data));
 				dispatch(setUserLoading(false));
 			} catch (error) {
-				// console.log(error);
 				 console.log(error);
+				 //console.log(error);
 				dispatch(setUser(null));
 				dispatch(setUserLoading(false));
 			}
